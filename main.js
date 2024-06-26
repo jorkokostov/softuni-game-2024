4dbe98086e92d53395df62ac9e30750afd28bc1f
jorkokostov <jorkokostov@gmail.com>
2024-06-26 10:13:50 +0300

    initialize game structure

---
+++ main.js
@@ -0,0 +1,57 @@
+//State initialization
+const state = {
+  player: 'Gosho',
+  wizzard: {
+    x: 100,
+    y: 100,
+    width: 50,
+    height: 50,
+  }
+};
+  
+//Game object creation
+const gameArea = document.querySelector('.game-area')
+const factory = {
+  createWizard(wizard){
+    //create element
+    const wizardElement = document.createElement('div');
+
+    //set styles
+    wizardElement.style.width = wizard.width + 'px';
+    wizardElement.style.height = wizard.height + 'px';
+    wizardElement.style.backgroundImage = 'url("images/wizard.png")';
+    wizardElement.style.backgroundSize = 'contain';
+    wizardElement.style.backgroundRepeat = 'no-repeat';
+    wizardElement.style.backgroundPosition = 'center';
+
+   
+
+    //set position
+    wizardElement.style.position = 'absolute';
+    wizardElement.style.left = wizard.x + 'px';
+    wizardElement.style.top = wizard.y + 'px';
+
+    //Attach to DOM
+    gameArea.appendChild(wizardElement);
+  }
+}
+
+//Input controll
+
+//Game loop
+
+
+//Game frames
+function newFrame() {
+
+}
+
+//Start game
+const startElement = document.querySelector('.game-start');
+startElement.addEventListener('click', (e) => {
+  //Hide start element
+  e.currentTarget.classList.add('hidden');
+
+  //Initializa game
+  factory.createWizard(state.wizzard)
+})
